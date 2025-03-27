4f038e38a9d350f78c52cd1371b1510b2fcb3e7a
Manuh08 <manuelasoarespacheco11@gmail.com>
2025-03-27 16:07:17 -0400

    tela de login certa

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { auth } from './config/firebaseConfig';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import './App.css/style.css'
 
 export default function App() {
   const [email, setEmail] = useState('');
@@ -18,16 +19,27 @@ export default function App() {
 
   return (
     <main>
+      <div className='Container'>
+        <div className='form'>
+          <h1>Login</h1>
+        </div>
       <form onSubmit={autenticarComFirebase}>
+        <div className='email'>
         <label htmlFor="email">E-mail:</label>
+        <div className='grupo box'>
+          <div className='caixa-email'>
         <input
           id="email"
           name="email"
+          placeholder='manuzinha@gmail.com'
           type="email"
           value={email}
           onChange={(evento) => setEmail(evento.target.value)}
         />
+        </div>
+        </div>
         <label htmlFor="password">Senha:</label>
+        <div className='caixa-senha'>
         <input
           id="password"
           name="password"
@@ -35,8 +47,15 @@ export default function App() {
           value={senha}
           onChange={(evento) => setSenha(evento.target.value)}
         />
+        </div>
+        </div>
+        <div className='Login'>
         <button type="submit">Entrar</button>
+        </div>
+
       </form>
+      </div>
+
     </main>
   );
 }
